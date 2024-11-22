2fb31d3706f27a071a477888313b46a8c0596ad3
Steve Saylor <steven.saylor@pitt.edu>
2024-11-21 20:13:15 -0500

    added output folder

---
+++ out/_next/static/chunks/pages/layout-bcf307966739262d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[482],{3052:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/layout",function(){return u(1449)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(3052)),_N_E=_.O()}]);
